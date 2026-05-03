a44fbd2704c55d331e085dfbabb1ce422744cbb3
rounakk001 <rounaksingh2303@gmail.com>
2026-05-04 02:10:13 +0530

    updated

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Rounak with React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+  return (
+    <h1>Rounak with Vitereact</h1>
+  )
+}
+
+export default App
+  
